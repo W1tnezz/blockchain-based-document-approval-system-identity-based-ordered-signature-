95c4f5df559de4a82c3859d09888f15395235066
983197214@qq.com <983197214@qq.com>
2023-12-26 10:32:59 +0800

    init go module

--- pubKeyGenerator/cmd/pubKeyGenerator/main.go
+++ pubKeyGenerator/cmd/pubKeyGenerator/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main()  {
+import (
+
 	
+	"os"
+	"os/signal"
+
+)
+
+func main() {
+
+
+	go func() {
+
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
+
+	// node.Stop()
 }
