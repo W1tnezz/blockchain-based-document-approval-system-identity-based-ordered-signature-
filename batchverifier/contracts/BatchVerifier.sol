6a55e0f6c3ed05c93a19ce93309ca5cd596aa22b
983197214@qq.com <983197214@qq.com>
2023-12-26 10:38:10 +0800

    init

---
+++ batchverifier/contracts/BatchVerifier.sol
@@ -0,0 +1,13 @@
+
+pragma solidity ^0.8.13;
+
+contract BatchVerifier {
+
+
+	
+	mapping (address => uint) balances;
+
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+
+}
