1ca7bd45eb40595466e6887fd21b9c36dd0ca96d
983197214@qq.com <983197214@qq.com>
2023-12-26 16:44:49 +0800

    test

---
+++ generator/cmd/generators/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+)
+
+func main() {
+
+
+	go func() {
+
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
+
+	// node.Stop()
+}
