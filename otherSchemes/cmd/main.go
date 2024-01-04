db5cea5b0af9c5cf309417122b70817a9a64414d
983197214@qq.com <983197214@qq.com>
2024-01-04 19:09:01 +0800

    gopbc

--- otherSchemes/cmd/main.go
+++ otherSchemes/cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"otherSchemes/pkg/schemes"
+	"log"
+	"os"
 
 	"github.com/Nik-U/pbc"
 )
 
 func main() {
-
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(os.Stdout)
+	
 	// Initialize group and the generator
 	params := pbc.GenerateA(160, 512)
 	pairing := params.NewPairing()
@@ -17,9 +20,11 @@ func main() {
 	msk := pairing.NewZr().Rand()
 	mpk := pairing.NewG1().PowZn(g, msk)
 
-	schemes.Liu(pairing, g, msk, mpk)
-	schemes.OMS()
-	schemes.WSA()
+	log.Println(mpk)
+
+	// schemes.Liu(pairing, g, msk, mpk)
+	// schemes.OMS()
+	// schemes.WSA()
 
 	// a := pairing.NewZr()
 
