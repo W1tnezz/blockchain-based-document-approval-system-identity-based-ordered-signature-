c13e4e00c11a95dd151e803b99597a641924dbe8
983197214@qq.com <983197214@qq.com>
2024-01-05 20:59:17 +0800

    11

--- otherSchemes/cmd/main.go
+++ otherSchemes/cmd/main.go
@@ -31,35 +31,45 @@ func main() {
 func sakaiExperiment(pairing *pbc.Pairing, msk *pbc.Element, mpk *pbc.Element, gen *pbc.Element, signerNum int) {
 	if signerNum > 0{
 		log.Printf("开始测试Sakai签名开销, 签名人数: %d", signerNum)
-		signerSet := make([]*schemes.SakaiKey, 0)
-		for i := 0; i < signerNum; i++ {
-			key := schemes.SakaiKeyGen(pairing, msk, gen)
-			signerSet = append(signerSet, key)
-		}
-	
-		sigSet := make([][2]*pbc.Element, 0)
-		message := []byte("TestSakai")
-
-		begin := time.Now()
-		firstSig := signerSet[0].SakaiSign(message)
-		cost := time.Since(begin)
-		log.Printf("第1位签名者签名耗时: %d microseconds", cost.Microseconds())
-		sigSet = append(sigSet, firstSig)
+		signCosts := make([]int, signerNum)
 		
-		for i := 1; i < signerNum; i++{
-			var sig [2]*pbc.Element
-			var err error
-
-			beginTime := time.Now()
-			sig, message, err = signerSet[i].SequentialSign(sigSet[i - 1], message, mpk, signerSet[i - 1].PublicKeyPoint, i + 1)
-			cost = time.Since(beginTime)
-
-			if err != nil {
-				log.Fatal("Error: %w", err)
-				return
+		for i := 0; i < 50; i++{
+			signerSet := make([]*schemes.SakaiKey, 0)
+			for i := 0; i < signerNum; i++ {
+				key := schemes.SakaiKeyGen(pairing, msk, gen)
+				signerSet = append(signerSet, key)
 			}
-			log.Printf("第%d位签名者签名耗时: %d microseconds", i + 1, cost.Microseconds())
-			sigSet = append(sigSet, sig)
+		
+			sigSet := make([][2]*pbc.Element, 0)
+			message := []byte("TestSakai")
+	
+			begin := time.Now()
+			firstSig := signerSet[0].SakaiSign(message)
+			cost := time.Since(begin)
+			signCosts[0] += int(cost.Microseconds())
+	
+			sigSet = append(sigSet, firstSig)
+			
+			for i := 1; i < signerNum; i++{
+				var sig [2]*pbc.Element
+				var err error
+	
+				beginTime := time.Now()
+				sig, message, err = signerSet[i].SequentialSign(sigSet[i - 1], message, mpk, signerSet[i - 1].PublicKeyPoint, i + 1)
+				cost = time.Since(beginTime)
+	
+				if err != nil {
+					log.Fatal("Error: %w", err)
+					return
+				}
+				signCosts[i] += int(cost.Microseconds())
+				sigSet = append(sigSet, sig)
+			}
+		}
+
+		for i := 0; i < signerNum; i++{
+			signCosts[i] = signCosts[i] / 50
+			log.Printf("50次实验, 第%d位签名者的平均签名开销: %d microseconds", i + 1, signCosts[i])
 		}
 		return
 	} 
